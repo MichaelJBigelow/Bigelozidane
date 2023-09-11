2028532734e33ed34b4501279cd620cf74582f53
Michael Bigelow <procureeminence@gmail.com>
2023-09-11 13:17:40 -0400

    FEAT: Add first encoding unit test

---
+++ app/src/test/java/com/bigelowinventions/bigelozidane/CipherUnitTests.kt
@@ -0,0 +1,24 @@
+package com.bigelowinventions.bigelozidane
+
+import com.bigelowinventions.bigelozidane.logic.Cipher
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Unit tests for the Cipher class.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class CipherUnitTests {
+    @Test
+    fun encode_pass() {
+        // Manual text entry
+        val inputText = "Sample text to process."
+        val encodedInput = "Jipmqy hydh ha mkanyjj"
+        assertEquals(encodedInput, Cipher.encode(inputText))
+
+        // cipher character sweep
+        //assertEquals(4, 2 + 2)
+    }
+}
