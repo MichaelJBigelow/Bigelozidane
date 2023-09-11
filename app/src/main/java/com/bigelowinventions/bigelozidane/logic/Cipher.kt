95c245bbdde476970c844c268682b61ed4b9447c
Michael Bigelow <procureeminence@gmail.com>
2023-09-11 12:41:17 -0400

    FEAT: Add cipher key from the original Bigelozidane program

--- app/src/main/java/com/bigelowinventions/bigelozidane/logic/Cipher.kt
+++ app/src/main/java/com/bigelowinventions/bigelozidane/logic/Cipher.kt
@@ -5,6 +5,35 @@ class Cipher {
 
     companion object {
 
+        val cipherKey = arrayOf(
+            mapOf("decoded" to "a", "encoded" to "i"),
+            mapOf("decoded" to "b", "encoded" to "z"),
+            mapOf("decoded" to "c", "encoded" to "n"),
+            mapOf("decoded" to "d", "encoded" to "x"),
+            mapOf("decoded" to "e", "encoded" to "y"),
+            mapOf("decoded" to "f", "encoded" to "w"),
+            mapOf("decoded" to "g", "encoded" to "v"),
+            mapOf("decoded" to "h", "encoded" to "t"),
+            mapOf("decoded" to "i", "encoded" to "u"),
+            mapOf("decoded" to "j", "encoded" to "s"),
+            mapOf("decoded" to "k", "encoded" to "r"),
+            mapOf("decoded" to "l", "encoded" to "q"),
+            mapOf("decoded" to "m", "encoded" to "p"),
+            mapOf("decoded" to "n", "encoded" to "c"),
+            mapOf("decoded" to "o", "encoded" to "a"),
+            mapOf("decoded" to "p", "encoded" to "m"),
+            mapOf("decoded" to "q", "encoded" to "l"),
+            mapOf("decoded" to "r", "encoded" to "k"),
+            mapOf("decoded" to "s", "encoded" to "j"),
+            mapOf("decoded" to "t", "encoded" to "h"),
+            mapOf("decoded" to "u", "encoded" to "e"),
+            mapOf("decoded" to "v", "encoded" to "g"),
+            mapOf("decoded" to "w", "encoded" to "f"),
+            mapOf("decoded" to "x", "encoded" to "d"),
+            mapOf("decoded" to "y", "encoded" to "o"),
+            mapOf("decoded" to "z", "encoded" to "b")
+        )
+
         fun encode(plainText: String): String {
             var encodedText = plainText
 
