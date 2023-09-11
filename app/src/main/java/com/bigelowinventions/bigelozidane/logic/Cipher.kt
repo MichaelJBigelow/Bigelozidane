7e94aaabf2eae62566df7b9e72e7445f70f025e2
Michael Bigelow <procureeminence@gmail.com>
2023-09-11 12:29:30 -0400

    FEAT: Add Cipher class framework

---
+++ app/src/main/java/com/bigelowinventions/bigelozidane/logic/Cipher.kt
@@ -0,0 +1,22 @@
+package com.bigelowinventions.bigelozidane.logic
+
+// Simple substitution cipher
+class Cipher {
+
+    companion object {
+
+        fun encode(plainText: String): String {
+            var encodedText = plainText
+
+            return encodedText
+        }
+
+        fun decode(encodedText: String): String {
+            var plainText = encodedText
+
+            return plainText
+        }
+
+    }
+
+}
