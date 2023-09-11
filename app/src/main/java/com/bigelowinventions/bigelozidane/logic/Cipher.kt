cf685ed40130e12006d8bb1825c463f80dbeb034
Michael Bigelow <procureeminence@gmail.com>
2023-09-11 14:43:55 -0400

    FIX: String concatenation in Cipher.encode

--- app/src/main/java/com/bigelowinventions/bigelozidane/logic/Cipher.kt
+++ app/src/main/java/com/bigelowinventions/bigelozidane/logic/Cipher.kt
@@ -54,7 +54,7 @@ class Cipher {
 
                         }
 
-                        encodedText.plus(encodedCharacter)
+                        encodedText += encodedCharacter
 
                         return@cipherLoop
                     }
