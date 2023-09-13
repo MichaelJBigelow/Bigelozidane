5c26aec3f3c363f443006572a12fd1102548038b
Michael Bigelow <procureeminence@gmail.com>
2023-09-13 03:07:35 -0400

    FEAT: Update ViewModel to handle new text fields

--- app/src/main/java/com/bigelowinventions/bigelozidane/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/bigelowinventions/bigelozidane/ui/home/HomeViewModel.kt
@@ -5,9 +5,6 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class HomeViewModel : ViewModel() {
-
-    private val _text = MutableLiveData<String>().apply {
-        value = "Bigelozidane"
-    }
-    val text: LiveData<String> = _text
+    val inputText: LiveData<String> = MutableLiveData<String>()
+    val outputText: LiveData<String> = MutableLiveData<String>()
 }
