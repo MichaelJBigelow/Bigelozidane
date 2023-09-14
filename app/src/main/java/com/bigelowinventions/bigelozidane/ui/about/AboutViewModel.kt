c1499c058596328a5e67121e6933842d0f0b639b
Michael Bigelow <procureeminence@gmail.com>
2023-09-14 14:03:43 -0400

    REFACTOR: Rename GalleryFragment to AboutFragment

--- app/src/main/java/com/bigelowinventions/bigelozidane/ui/about/AboutViewModel.kt
+++ app/src/main/java/com/bigelowinventions/bigelozidane/ui/about/AboutViewModel.kt
@@ -1,10 +1,10 @@
-package com.bigelowinventions.bigelozidane.ui.gallery
+package com.bigelowinventions.bigelozidane.ui.about
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class GalleryViewModel : ViewModel() {
+class AboutViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is gallery Fragment"
