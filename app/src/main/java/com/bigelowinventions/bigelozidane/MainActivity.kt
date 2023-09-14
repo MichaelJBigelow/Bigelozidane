723f5c8543f699cd311fddc204453600bd52c85a
Michael Bigelow <procureeminence@gmail.com>
2023-09-14 14:08:52 -0400

    REFACTOR: Rename SlideshowFragment to LinksFragment

--- app/src/main/java/com/bigelowinventions/bigelozidane/MainActivity.kt
+++ app/src/main/java/com/bigelowinventions/bigelozidane/MainActivity.kt
@@ -37,7 +37,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_translator, R.id.nav_about, R.id.nav_slideshow
+                R.id.nav_translator, R.id.nav_about, R.id.nav_links
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
--- app/src/main/java/com/bigelowinventions/bigelozidane/ui/links/LinksViewModel.kt
+++ app/src/main/java/com/bigelowinventions/bigelozidane/ui/links/LinksViewModel.kt
@@ -1,10 +1,10 @@
-package com.bigelowinventions.bigelozidane.ui.slideshow
+package com.bigelowinventions.bigelozidane.ui.links
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class SlideshowViewModel : ViewModel() {
+class LinksViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is slideshow Fragment"
