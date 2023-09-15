d59c118a04e8548750e17a3036f4888d6c704343
Michael Bigelow <procureeminence@gmail.com>
2023-09-14 21:04:27 -0400

    FEAT: Set default translator value to empty String

--- app/src/main/java/com/bigelowinventions/bigelozidane/ui/translator/TranslatorViewModel.kt
+++ app/src/main/java/com/bigelowinventions/bigelozidane/ui/translator/TranslatorViewModel.kt
@@ -1,14 +1,14 @@
 package com.bigelowinventions.bigelozidane.ui.translator
 
 import androidx.annotation.MainThread
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.bigelowinventions.bigelozidane.logic.Cipher
 
 class TranslatorViewModel : ViewModel() {
-    val inputText: MutableLiveData<String> = MutableLiveData<String>()
-    val outputText: MutableLiveData<String> = MutableLiveData<String>()
+    val inputText: MutableLiveData<String> = MutableLiveData<String>("")
+    val outputText: MutableLiveData<String> = MutableLiveData<String>("")
+
     @MainThread
     fun encode() {
         outputText.postValue(
