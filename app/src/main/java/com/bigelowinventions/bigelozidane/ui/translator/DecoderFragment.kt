d6e7fa3a761e608faacd0028822912fe39475542
Michael Bigelow <procureeminence@gmail.com>
2023-10-12 13:49:31 -0400

    FEAT: Add copy/paste logic to DecoderFragment

--- app/src/main/java/com/bigelowinventions/bigelozidane/ui/translator/DecoderFragment.kt
+++ app/src/main/java/com/bigelowinventions/bigelozidane/ui/translator/DecoderFragment.kt
@@ -1,14 +1,18 @@
 package com.bigelowinventions.bigelozidane.ui.translator
 
+import android.content.ClipboardManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
+import android.widget.ImageView
+import androidx.core.content.ContextCompat
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.bigelowinventions.bigelozidane.databinding.FragmentDecoderBinding
+import com.bigelowinventions.bigelozidane.logic.InputManager
 
 class DecoderFragment : Fragment() {
 
@@ -34,9 +38,26 @@ class DecoderFragment : Fragment() {
             decoderViewModel.inputText.value = text.toString()
         }
 
+        val pasteTextImageButton: ImageView = binding.pasteTextImageButton
+        pasteTextImageButton.setOnClickListener {
+            val context = requireContext()
+            val clipboard = ContextCompat.getSystemService(
+                context,
+                ClipboardManager::class.java
+            ) as ClipboardManager
+            binding.inputEditText.setText(InputManager.readFromClipboard(clipboard))
+        }
+
         val outputEditText: EditText = binding.outputEditText
         decoderViewModel.outputText.observe(viewLifecycleOwner) {
             outputEditText.setText(it)
+        }
+
+        val copyTextImageButton: ImageView = binding.copyTextImageButton
+        copyTextImageButton.setOnClickListener {
+            val context = requireContext()
+            val clipboard = ContextCompat.getSystemService(context, ClipboardManager::class.java) as ClipboardManager
+            InputManager.writeToClipboard(decoderViewModel.outputText.value.toString(), clipboard)
         }
 
         binding.translateButton.setOnClickListener {
